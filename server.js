645893f84999bdcd218bf1863274a3b9b4548721
Hao Gao <hao.x.gao@gmail.com>
2013-02-25 03:10:13 -0500

    format server.js

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ var app = express();
 var server = http.createServer(app);
 var io = require('socket.io').listen(server);
 var redis = require("redis"),
-        rc = redis.createClient();
+  rc = redis.createClient();
 
 app.set('view engine', 'jade');
-app.set('view options', { layout: true });
+app.set('view options', {
+  layout: true
+});
 app.set('views', __dirname + '/views');
 
 app.use(express.static(__dirname + '/static')); //why app.use('/static', express.static(__dirname + '/static'));?
@@ -18,77 +20,77 @@ io.sockets.on('connection', function(socket) {
 
   var sendChat = function(text, created_at, link) {
     socket.emit('chat', {
-      text : text,
-      created_at : created_at,
-      link : link
+      text: text,
+      created_at: created_at,
+      link: link
     });
   };
-  
+
   var reloadVideos = function() {
-    rc.zcard('vine:link:realtime', function (err, count) {
-      rc.zrange('vine:link:realtime', count - 20, count - 1, function (err, replies) {
+    rc.zcard('vine:link:realtime', function(err, count) {
+      rc.zrange('vine:link:realtime', count - 20, count - 1, function(err, replies) {
         for (var i = 0; i < replies.length - 1; i++) {
-          
-          rc.hvals(replies[i], function (err, replies2) {
+
+          rc.hvals(replies[i], function(err, replies2) {
             var msg = [];
             for (var j = 0; j < replies2.length; j++) {
               msg.push(replies2[j]);
             }
             msgs.push(msg);
             console.log(msgs.length);
-            
+
           });
-          
+
         }
 
-        rc.hvals(replies[replies.length - 1], function (err, replies2) {
+        rc.hvals(replies[replies.length - 1], function(err, replies2) {
           var msg = [];
           for (var j = 0; j < replies2.length; j++) {
             msg.push(replies2[j]);
           }
-          
+
           sendChat(msg[0], msg[1], msg[2]);
         });
       });
     });
   }
-  
+
   reloadVideos();
 
-  socket.on('chat', function(data){
-  
-      if (msgs.length != 0) {
-        console.log(msgs.length);
-          msg = msgs.pop()
-          sendChat(msg[0], msg[1], msg[2]);
-      } else {
-        rc.zcard('vine:link:realtime', function (err, count) {
-          rc.zrange('vine:link:realtime', count - 20, count - 1, function (err, replies) {
-            for (var i = 0; i < replies.length - 1; i++) {
-              
-              rc.hvals(replies[i], function (err, replies2) {
-                var msg = [];
-                for (var j = 0; j < replies2.length; j++) {
-                  msg.push(replies2[j]);
-                }
-                msgs.push(msg);
-                console.log(msgs.length);
-                
-              });
-              
-            }
+  socket.on('chat', function(data) {
 
-            rc.hvals(replies[replies.length - 1], function (err, replies2) {
+    if (msgs.length != 0) {
+      console.log(msgs.length);
+      msg = msgs.pop()
+      sendChat(msg[0], msg[1], msg[2]);
+    } else {
+      rc.zcard('vine:link:realtime', function(err, count) {
+        rc.zrange('vine:link:realtime', count - 20, count - 1, function(err, replies) {
+          for (var i = 0; i < replies.length - 1; i++) {
+
+            rc.hvals(replies[i], function(err, replies2) {
               var msg = [];
               for (var j = 0; j < replies2.length; j++) {
                 msg.push(replies2[j]);
               }
-              
-              sendChat(msg[0], msg[1], msg[2]);
+              msgs.push(msg);
+              console.log(msgs.length);
+
             });
+
+          }
+
+          rc.hvals(replies[replies.length - 1], function(err, replies2) {
+            var msg = [];
+            for (var j = 0; j < replies2.length; j++) {
+              msg.push(replies2[j]);
+            }
+
+            sendChat(msg[0], msg[1], msg[2]);
           });
         });
-      }
+      });
+    }
   });
 });
 
@@ -96,7 +98,7 @@ app.get('/random', function(req, res, next) {
   res.render('video');
 });
 
-app.get('/?', function(req, res){
+app.get('/?', function(req, res) {
   res.render('index');
 });
 
